125d2038737b948f194b2a7277c49208d901eecf
Hiwarkarneha <98576968+Hiwarkarneha@users.noreply.github.com>
2022-01-28 15:43:01 +0530

    Add files via upload

---
+++ CollectionDemo.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+  
+class CollectionDemo {
+        public static void main(String[] args) {
+            int arr[]= new int[] {1, 2, 3, 4};
+            Vector<Integer> v = new Vector();
+            Hashtable<Integer , String> h = new Hashtable();
+    
+            v.addElement(1);
+            v.addElement(2);
+    
+            h.put(1, "Neha");
+            h.put(2, "4Neha");
+    
+            System.out.println(arr[0]);
+            System.out.println(v.elementAt(0));
+            System.out.println(h.get(1));
+        }
+    
+    }
+    
+
